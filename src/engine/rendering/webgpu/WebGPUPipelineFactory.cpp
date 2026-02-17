c6649a38ebcb9d1407a88dbd2f543b5fe1d00355
KaNaDaAT <56671994+KaNaDaAT@users.noreply.github.com>
2026-02-17 23:42:21 +0100

    Fix Layut order

--- src/engine/rendering/webgpu/WebGPUPipelineFactory.cpp
+++ src/engine/rendering/webgpu/WebGPUPipelineFactory.cpp
@@ -116,7 +116,12 @@ std::shared_ptr<WebGPUPipeline> WebGPUPipelineFactory::createRenderPipeline(
 	std::vector<wgpu::BindGroupLayout> layoutArray;
 	layoutArray.reserve(layouts.size());
 	for (const auto &layoutInfo : layouts)
-		layoutArray.push_back(layoutInfo->getLayout());
+	{
+		if (layoutInfo)
+			layoutArray.push_back(layoutInfo->getLayout());
+		else
+			layoutArray.push_back(nullptr); // Preserve index for unused bind groups
+	}
 
 	wgpu::PipelineLayout layout = createPipelineLayout(layoutArray.data(), static_cast<uint32_t>(layoutArray.size()));
 	desc.layout = layout;
